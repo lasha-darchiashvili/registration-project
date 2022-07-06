7c5e7a0d020ba1cb47fe153d7c6211adda6a8fb7
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2022-07-06 14:53:13 +0400

    popup in progress

--- page2.js
+++ page2.js
@@ -4,21 +4,36 @@ const username = document.querySelector(".username");
 const email = document.querySelector(".email");
 const telephone = document.querySelector(".tel");
 const birthDate = document.querySelector(".form-control");
+const popup = document.querySelector(".popup");
+const warningText = document.querySelector(".warning-text");
+const errorMessage = document.querySelector(".error-message");
+const closePopup = document.querySelector(".close-popup");
 
 // email info
 const emailEnding = "@redberry.ge";
 const emailEndingLength = emailEnding.length;
+
+const validation = {
+  username: false,
+  email: false,
+  telephone: false,
+  birthDate: false,
+};
 
 submitInfo.addEventListener("click", function (e) {
   e.preventDefault();
 
   //   username validation
   if (username.value.trim() == "" || username.value.trim().length < 2) {
+    validation.username = false;
     username.parentElement.classList.add("invalid");
     username.parentElement.classList.remove("valid");
+
+    displayPopup("Invalid Username", "Please enter a valid Username");
   } else {
     username.parentElement.classList.remove("invalid");
     username.parentElement.classList.add("valid");
+    validation.username = true;
   }
 
   //   taking last 12 characters of email value which should match "@redberry.ge"
@@ -31,30 +46,48 @@ submitInfo.addEventListener("click", function (e) {
 
   //   email validation
   if (emailValueEnding !== emailEnding || !testEmail(email.value.trim())) {
+    validation.email = false;
     email.parentElement.classList.add("invalid");
     email.parentElement.classList.remove("valid");
+    if (validation.username) {
+      displayPopup("Invalid Email", "Please enter a valid email");
+    }
   } else {
     email.parentElement.classList.remove("invalid");
     email.parentElement.classList.add("valid");
+    validation.email = true;
   }
 
   const telValue = telephone.value;
   //   phone validation
   if (telValue === "" || telValue.length !== 9 || !testTelephone(telValue)) {
+    validation.telephone = false;
     telephone.parentElement.classList.add("invalid");
     telephone.parentElement.classList.remove("valid");
+    if (validation.email && validation.username) {
+      displayPopup(
+        "Invalid Telephone Number",
+        "Please enter a valid Telephone Number"
+      );
+    }
   } else {
     telephone.parentElement.classList.remove("invalid");
     telephone.parentElement.classList.add("valid");
+    validation.telephone = true;
   }
 
   if (!birthDate.value) {
     birthDate.parentElement.classList.add("invalid");
     birthDate.parentElement.classList.remove("valid");
+    if (validation.email && validation.username && validation.telephone) {
+      displayPopup("Invalid Birth Date", "Please enter a Birth Date");
+    }
   } else {
     birthDate.parentElement.classList.remove("invalid");
     birthDate.parentElement.classList.add("valid");
   }
+
+  //   location.href = "/experience.html";
 });
 
 // email regex
@@ -68,3 +101,17 @@ const testEmail = function (email) {
 const testTelephone = function (telephone) {
   return /^[0-9]*$/.test(telephone);
 };
+
+function displayPopup(inputWarningText, inputErrosMessage) {
+  warningText.textContent = inputWarningText;
+  errorMessage.textContent = inputErrosMessage;
+  popup.style.display = "block";
+}
+
+function hidePopup() {
+  popup.style.display = "none";
+}
+
+closePopup.addEventListener("click", function () {
+  popup.style.display = "none";
+});
